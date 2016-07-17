05201f2924d0550493b84f934f24f2e44e4e04e0
Doug Wright <dwright@takepart.com>
2016-07-16 18:24:30 -0700

    Sudoku: step 1; grid and 9 placement

--- sudoku.rb
+++ sudoku.rb
@@ -1,40 +1,61 @@
 class Sudoku
   def initialize
     @grid = Array.new(9).map{ |e| Array.new(9) }
-    @grid[0] = random_num_array
+    make_box_numbers(0,0)
+    make_box_numbers(0,3)
+    make_box_numbers(0,6)
+    make_box_numbers(3,0)
+    make_box_numbers(3,3)
+    make_box_numbers(3,6)
+    make_box_numbers(6,0)
+    make_box_numbers(6,3)
+    make_box_numbers(6,6)
   end
 
-  def build
-    [1,2,3,4,5,6,7,8].each do |ind|
-      sorted_placement(ind)
-    end
-    p @grid
-  end
-
-  def sorted_placement(index)
-    new_nums = random_num_array
-
-    while new_nums.size > 0
-      counter = new_nums.size - 1
-
-      # if @grid[index][counter].nil?
-
-        new_nums.each do |num|
-          unless @grid[index-1][counter] == num
-            @grid[index][counter] = num
-            new_nums.delete(num)
-            break
-          end
-        end
-
-      # end
-    end
+  def make_box_numbers(x,y) # send left corners for params: 0,0 - 0,3 - 6,5 etc.
+    numbers = one_thru_nine.shuffle
+    @grid[x][y]     = numbers.pop
+    @grid[x][y+1]   = numbers.pop
+    @grid[x][y+2]   = numbers.pop
+    @grid[x+1][y]   = numbers.pop
+    @grid[x+1][y+1] = numbers.pop
+    @grid[x+1][y+2] = numbers.pop
+    @grid[x+2][y]   = numbers.pop
+    @grid[x+2][y+1] = numbers.pop
+    @grid[x+2][y+2] = numbers.pop
   end
 
 
-  # private
 
-  def random_num_array
-    [1,2,3,4,5,6,7,8,9].shuffle
+  def get_y_numbers(x,y)
+    @grid.map{ |row| row[y] }.compact
+  end
+
+  def get_x_numbers(x)
+    @grid[x].compact
+  end
+
+  def one_thru_nine
+    (1..9).map{|f| f}
+  end
+
+  def print_grid
+    puts "~~~~~~~~~~~ Sudoku! ~~~~~~~~~~~"
+    @grid.each{ |row| p row }
+    puts "^^^^^^^^^^^ Sudoku! ^^^^^^^^^^^"
   end
 end
+
+#         y
+#        0     1    2    3    4    5    6     7    8
+#     0 [ 1 ,  2 ,  3 , nil, nil, nil, nil, nil, nil]
+#     1 [ 9 ,  8 ,  7 , nil, nil, nil, nil, nil, nil]
+#     2 [nil, nil,    , nil, nil, nil, nil, nil, nil]
+
+#     3 [nil, nil,    , nil, nil, nil, nil, nil, nil]
+#     4 [y-2, y-1,  42, nil, nil, nil, nil, nil, nil]
+#     5 [y-1,  51, y+1, nil, nil, nil, nil, nil, nil]
+
+#     6 [nil, nil, nil, nil, nil, nil, nil, nil, nil]
+#     7 [nil, nil, nil, nil, nil, nil, nil, nil, nil]
+#  x  8 [nil, nil, nil, nil, nil, nil, nil, nil, nil]
