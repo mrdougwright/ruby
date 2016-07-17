77d31d063b00791940d1f1c66b4741044dd28f32
Doug Wright <dwright@takepart.com>
2016-07-16 22:22:36 -0700

    Sudoku: last failed attempt with array of 9s

--- sudoku.rb
+++ sudoku.rb
@@ -1,9 +1,12 @@
 class Sudoku
+
   def initialize
     @grid = Array.new(9).map{ |e| Array.new(9) }
-    make_box_numbers(0,0)
-    make_box_numbers(0,3)
-    make_box_numbers(0,6)
+
+    (1..9).to_a.shuffle.each {|num| nine_inserts(num) }
+    # make_box_numbers(0,0)
+    # make_box_numbers(0,3)
+    # make_box_numbers(0,6)
     # make_box_numbers(3,0)
     # make_box_numbers(3,3)
     # make_box_numbers(3,6)
@@ -12,55 +15,69 @@ class Sudoku
     # make_box_numbers(6,6)
   end
 
+  def nine_inserts(number)
+    x=0; y=0;
+    [0,3,6].each do |x_row|
+      [0,3,6].each do |y_row|
+        # puts "Grid box: #{x_row}, #{y_row}"
+        test_and_insert_nums(x_row, y_row, number)
+      end
+    end
+  end
+
+  def test_and_insert_nums(x_row, y_row, number)
+    number_inserted = false
+    3.times do |xdex|
+      break if number_inserted
+      3.times do |ydex|
+        number_inserted = insert_number(x_row + xdex, y_row + ydex, number)
+        break if number_inserted
+      end
+    end
+  end
+
   def insert_number(x, y, num)
     return false unless @grid[x][y].nil?
-    if ( !get_x_numbers(x).include?(num) && !get_y_numbers(y).include?(num) )
+    if ( !x_nums(x).include?(num) && !y_nums(y).include?(num) )
       @grid[x][y] = num
       return true
     end
   end
 
-  def make_box_numbers(x,y)
-    # return unless modulo 3, 0 for x, y ...
-    numbers = one_thru_nine.shuffle
-
-    numbers.each do |number|
-      number_inserted = false
-      3.times do |xdex|
-        break if number_inserted
-        3.times do |ydex|
-          number_inserted = insert_number(x + xdex, y + ydex, number)
-          break if number_inserted
-        end
+  def box_nums(x,y) # send left corner coordinates
+    box_numbers = []
+    3.times do |x_row|
+      3.times do |y_row|
+        # puts "Grid box: #{x_row}, #{y_row}"
+        box_numbers << @grid[x + x_row][y + y_row]
       end
     end
+    box_numbers.compact
   end
 
-
-  def get_y_numbers(y)
+  def y_nums(y)
     @grid.map{ |row| row[y] }.compact
   end
 
-  def get_x_numbers(x)
+  def x_nums(x)
     @grid[x].compact
   end
 
   def one_thru_nine
+    # Array.new(9){|x=1| x+=1}
     (1..9).map{|f| f}
   end
 
   def print_grid
-    puts "~~~~~~~~~~~ Sudoku! ~~~~~~~~~~~"
-    @grid.each{ |row| p row }
-    puts "^^^^^^^^^^^ Sudoku! ^^^^^^^^^^^"
+    puts "~~~~~~~~~ Sudoku! ~~~~~~~~~"
+      @grid.each{ |row| p row }
+    puts "^^^^^^^^^ Sudoku! ^^^^^^^^^"
   end
 end
 
-Sudoku.new.print_grid
-
 #         y
-#        0     1    2    3    4    5    6     7    8
-#     0 [ 1 ,  2 ,  3 , nil, nil, nil, nil, nil, nil]
+#         0    1    2    3    4    5    6     7    8
+#     0 [ c ,  b ,  a , nil, nil, nil, nil, nil, nil]
 #     1 [ 9 ,  8 ,  7 , nil, nil, nil, nil, nil, nil]
 #     2 [nil, nil,    , nil, nil, nil, nil, nil, nil]
 
@@ -71,3 +88,9 @@ Sudoku.new.print_grid
 #     6 [nil, nil, nil, nil, nil, nil, nil, nil, nil]
 #     7 [nil, nil, nil, nil, nil, nil, nil, nil, nil]
 #  x  8 [nil, nil, nil, nil, nil, nil, nil, nil, nil]
+
+
+# [ 1,  2,  3,   4,  5,  6,   7,  8,  9]
+# [10, 11, 12,  13, 14, 15,  16, 17, 18]
+# [19, 20, 21,  22, 23, 24,  25, 26, 27]
+
